254d78f2cc3e2eae164081bcf16a10bc87adc1e9
Francisco Cobas <fcarocena@gmail.com>
2019-11-18 21:53:29 -0300

    use-image prototype

--- src/components/sections/ProjectCard.js
+++ src/components/sections/ProjectCard.js
@@ -1,23 +1,48 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import { Link, useRouteMatch } from 'react-router-dom'
-
 import '../../styles/ProjectCard.scss'
 
-function ProjectCard(props) {
+let OnImagesLoaded = require('react-on-images-loaded');
 
-  let { url } = useRouteMatch();
 
-  const { project } = props;
+class ProjectCard extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      showImages: false
+    }
+  }
+  runAfterImagesLoaded(elements) {
+    console.log('hola')
+    console.log(elements)
+  }
+  runTimeoutFunction(elements) {
+    console.log('hola2')
+    console.log(elements)
+  }
+  render() {
 
-  return (
-    <div className='col-4 project-card-container text-center mt-4'>
-      <h4>{project.name}</h4>
-      <Link to={`${url}/${project.url}`}>
-        <img className='img-fluid' src={project.imgCover} alt={project.name} />
-      </Link>
-    </div>
-  )
+    let {project} = this.props
+
+    return (
+      <div className='col-4 project-card-container text-center mt-4'>
+        <h4>{project.name}</h4>
+        <Link to={`hola`}>
+
+          <OnImagesLoaded
+            onLoaded={this.runAfterImagesLoaded.bind(this)}
+            onTimeout={this.runTimeoutFunction.bind(this)}
+            timeout={7000}
+          >
+            {
+              <img className='img-fluid' src={project.imgCover} alt={project.name} />
+            }
+          </OnImagesLoaded>
+        </Link>
+      </div>
+    )
+  }
 }
 
 
