c65cc277025b36f84d14bc313ffa0d619bda3dc2
Francisco Cobas <fcarocena@gmail.com>
2019-11-18 22:40:37 -0300

    Improve Project Detail

--- src/api/api.js
+++ src/api/api.js
@@ -13,8 +13,17 @@ function getProjectDetail(name) {
   return new Promise((resolve) => {
     resolve({
       name: 'Cooperativa Comuna',
-      description: 'Comuna es una cooperativa de trabajo que integramos investigadoras/es de las ciencias económicas y que comienza a trabajar en setiembre de 2014. Nuestro propósito es generar conocimiento con organizaciones sociales, tanto con sindicatos como con cooperativas de trabajo y de vivienda así como otras organizaciones populares a fin de contribuir al pensamiento crítico y la acción colectiva. Intentamos sostener acuerdos de trabajo que nos permitan el despliegue de una estrategia que combina el aporte en tres grandes ejes: formación; elaboración de documentos de investigación y asesoramiento especializado.',
-      img: 'https://res.cloudinary.com/mepediacobas/image/upload/v1572376540/subte-web/comuna_logo.jpg'
+      // description: 'Comuna es una cooperativa de trabajo que integramos investigadoras/es de las ciencias económicas y que comienza a trabajar en setiembre de 2014. Nuestro propósito es generar conocimiento con organizaciones sociales, tanto con sindicatos como con cooperativas de trabajo y de vivienda así como otras organizaciones populares a fin de contribuir al pensamiento crítico y la acción colectiva. Intentamos sostener acuerdos de trabajo que nos permitan el despliegue de una estrategia que combina el aporte en tres grandes ejes: formación; elaboración de documentos de investigación y asesoramiento especializado.',
+      description: 'Comuna es una cooperativa de trabajo que integran investigadoras/es de las ciencias económicas y que comienza a trabajar en setiembre de 2014. Desde el comienzo de Subte nos interesa vincularnos con otras cooperativas y organizaciones, además de parecernos súper importante tener claridad de la economía de nuestra cooperativa y del mundo en general. Es por ello que nos interesó formar un vínculo con Comuna.',
+      img: 'https://res.cloudinary.com/mepediacobas/image/upload/v1572376540/subte-web/comuna_logo.jpg',
+      trabajosUrls: [
+        'https://via.placeholder.com/200',
+        'https://via.placeholder.com/200',
+        'https://via.placeholder.com/200',
+        'https://via.placeholder.com/200',
+        'https://via.placeholder.com/200',
+        'https://via.placeholder.com/200',
+      ]
     })
   })
 }
--- src/components/sections/projects/ProjectDetail.js
+++ src/components/sections/projects/ProjectDetail.js
@@ -4,20 +4,24 @@ import { useParams } from 'react-router-dom'
 
 import { getProjectDetail } from '../../../api/api'
 
+import '../../../styles/ProjectDetail.scss'
+
 export default function ProjectDetail() {
   let { id } = useParams();
 
   const [projectDetail, setProjectDetail] = useState(null)
+  const [trabajosUrls, setTrabajosUrls] = useState([])
 
   useEffect(() => {
     getProjectDetail(id).then((response) => {
 
       setProjectDetail(response)
+      setTrabajosUrls(response.trabajosUrls)
     })
   }, [id])
 
   return (
-    <div>
+    <div className='project-detail-container'>
       {
         projectDetail !== null ?
           <div className='text-center mt-3'>
@@ -26,10 +30,67 @@ export default function ProjectDetail() {
               <div className='col-4'>
                 <img className='img-fluid' src={projectDetail.img} alt={projectDetail.name} />
               </div>
-              <div className='col-5'>
+              <div className='col-5 text-left'>
                 <p>{projectDetail.description}</p>
               </div>
             </div>
+            <h2>Trabajos</h2>
+            {
+              trabajosUrls.length > 0 ?
+                <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+                  <ol className="carousel-indicators">
+                    {
+                      trabajosUrls.map((trabajoUrl, index) => {
+                        return (
+                          <li data-target="#carouselExampleIndicators" data-slide-to={index}></li>
+                        )
+                      })
+                    }
+
+                    {/* <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li> */}
+                  </ol>
+                  <div className="carousel-inner">
+                    {
+                      trabajosUrls.map((trabajoUrl, index) => {
+                        return (
+                          <div className="carousel-item">
+                            <img src={trabajoUrl} alt={`${projectDetail.name} work ${index}`} className='d-block w-100' />
+                          </div>
+                        )
+                      })
+                    }
+{/* 
+                    <div className="carousel-item">
+                      <img className="d-block w-100" src="..." alt="Second slide" />
+                    </div>
+                    <div className="carousel-item">
+                      <img className="d-block w-100" src="..." alt="Third slide" />
+                    </div> */}
+                  </div>
+                  <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="sr-only">Previous</span>
+                  </a>
+                  <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="sr-only">Next</span>
+                  </a>
+                </div>
+                // <div className='row carousel-container'>
+                //   {
+                //     trabajosUrls.map((trabajoUrl, index) => {
+                //       return (
+                //         <div className='col' key={index}>
+                //           <img src={trabajoUrl} alt={`${projectDetail.name} work ${index}`} className='' />
+                //         </div>
+                //       )
+                //     })
+                //   }
+                // </div>
+                :
+                <></>
+            }
           </div> :
           'No hay nada'
       }
