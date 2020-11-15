31c89a99c183e9826b2a82b89500394fea9bfecb
Francisco Cobas <fcarocena@gmail.com>
2020-11-15 10:20:03 -0300

    Refactor contact form

--- src/components/sections/Contact.js
+++ src/components/sections/Contact.js
@@ -1,140 +1,65 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useRef } from 'react';
 import { NavLink } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 
-import { useSignUpForm } from '../utils/CustomHooks';
 import { sendContactEmail } from '../../api/_api';
 
 import '../../styles/Contact.scss';
 
-/* eslint-disable no-control-regex */
-const emailRegex = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/;
-/* eslint-enable no-control-regex */
-
 const qs = require('qs');
+const loadingMessage = 'No cierre el navegador se está enviando el mensaje';
 
 const Contact = (props) => {
-  const [errors, setErrors] = useState({});
   const [userMessage, setUserMessage] = useState({});
   const [showSpinner, setShowSpinner] = useState(false);
-  const [spinnerText, setSpinnerText] = useState('');
   const [messageText] = useState(() =>
     qs.parse(props.location.search.slice(1)).subject != null
       ? qs.parse(props.location.search.slice(1)).subject
       : '',
   );
+  const { register, handleSubmit, errors } = useForm();
 
-  const spinnerTextRef = useRef(
-    'No cierre el navegador se está enviando el mensaje',
-  );
+  const spinnerTextRef = useRef(loadingMessage);
 
-  let stopper = `${spinnerTextRef.current}...`;
-
-  useEffect(() => {}, [spinnerText]);
-
-  useEffect(() => {}, []);
-
-  const onSubmitForm = async () => {
-    await validateForm();
+  const onSubmit = async (data, e) => {
     let interval;
-    if (
-      Object.keys(errors).length > 0 &&
-      !errors.name &&
-      !errors.email &&
-      !errors.message
-    ) {
-      try {
-        interval = setSendingIntervalEvent();
-        setShowSpinner(true);
-        const response = await sendContactEmail({
-          email: inputs.email,
-          name: inputs.name,
-          tel: inputs.tel,
-          text: inputs.message,
-          subject: inputs.subject,
+    try {
+      interval = setSendingIntervalEvent();
+      setShowSpinner(true);
+      const response = await sendContactEmail({
+        email: data.email,
+        name: data.name,
+        tel: data.tel,
+        text: data.message,
+        subject: data.subject,
+      });
+      if (response.status === 'ok') {
+        setUserMessage({
+          type: 'success',
+          message: `Tu mensaje fue enviado con éxito.`,
         });
-        if (response.status === 'ok') {
-          setUserMessage({
-            type: 'success',
-            message: `Tu mensaje fue enviado con éxito.`,
-          });
-          clearForm();
-        } else {
-          setUserMessage({
-            type: 'error',
-            message:
-              'Hubo un error al mandar el mail, intente de nuevo o comuniquese con el mail hola@subte.uy. ¡Gracias!',
-          });
-        }
-      } catch (err) {
-        console.log(err);
-      } finally {
-        setShowSpinner(false);
-        clearInterval(interval);
+        e.target.reset();
+      } else {
+        setUserMessage({
+          type: 'error',
+          message:
+            'Hubo un error al mandar el mail, intente de nuevo o comuniquese con el mail hola@subte.uy. ¡Gracias!',
+        });
       }
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setShowSpinner(false);
+      clearInterval(interval);
     }
   };
-
-  const { inputs, handleInputChange, handleSubmit } = useSignUpForm(
-    {
-      name: '',
-      tel: '',
-      email: '',
-      subject: '',
-      message: `${messageText}`,
-    },
-    onSubmitForm,
-  );
 
   const setSendingIntervalEvent = () => {
     return setInterval(() => {
-      spinnerTextRef.current === stopper
-        ? (spinnerTextRef.current =
-            'No cierre el navegador se está enviando el mensaje')
-        : (spinnerTextRef.current = `${spinnerTextRef.current}.`);
-
-      setSpinnerText(spinnerTextRef.current);
+      let text = spinnerTextRef.current;
+      text =
+        text === `${spinnerTextRef.current}...` ? loadingMessage : `${text}.`;
     }, 300);
-  };
-
-  const validateForm = async (inputName) => {
-    let obj = {};
-    switch (inputName) {
-      case 'name':
-        obj = {
-          ...errors,
-          name: inputs[inputName].length < 3,
-        };
-        break;
-      case 'email':
-        obj = {
-          ...errors,
-          email: !emailRegex.exec(inputs[inputName]),
-        };
-        break;
-      case 'message':
-        obj = {
-          ...errors,
-          message: inputs[inputName].length < 10,
-        };
-        break;
-      default:
-        obj = {
-          ...errors,
-          name: inputs.name.length < 3,
-          email: !emailRegex.exec(inputs.email),
-          message: inputs.message.length < 10,
-        };
-        break;
-    }
-    await setErrors(obj);
-  };
-
-  const clearForm = () => {
-    handleInputChange({ custom: true, name: 'name', value: '' });
-    handleInputChange({ custom: true, name: 'email', value: '' });
-    handleInputChange({ custom: true, name: 'tel', value: '' });
-    handleInputChange({ custom: true, name: 'subject', value: '' });
-    handleInputChange({ custom: true, name: 'message', value: '' });
   };
 
   return (
@@ -143,113 +68,53 @@ const Contact = (props) => {
         <div className="col-11">
           <div className="row justify-content-center mb-5">
             <div className="col-md-6">
-              <form
-                className="row mb-3 justify-content-left"
-                onSubmit={handleSubmit}
-                autoComplete="on"
-                noValidate
-              >
-                <div className="col-12 col-md-10">
-                  <label className="d-block">Nombre y Apellido:</label>
-                  <input
-                    name="name"
-                    type="text"
-                    className="w-100"
-                    required
-                    onChange={handleInputChange}
-                    value={inputs.name}
-                    onBlur={() => validateForm('name')}
-                  />
-                </div>
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <label>Nombre y Apellido: *</label>
+                <input
+                  name="name"
+                  id="name"
+                  type="text"
+                  ref={register({ required: true })}
+                  defaultValue={messageText}
+                />
                 {errors.name && (
-                  <div className="col-12 col-md-10 mt-2 error">
+                  <div className="error">
                     <span>
                       El nombre y apellido tiene que tener al menos{' '}
                       <span className="negrita">3 letras</span>
                     </span>
                   </div>
                 )}
-                <div className="w-100"></div>
-                <div className="col-12 col-md-10 mt-2">
-                  <div className="row justify-content-between">
-                    <div className="col-6">
-                      <label>Teléfono:</label>
-                    </div>
-                    <div className="col-4 text-right italica subte-font-sm">
-                      <span>Opcional</span>
-                    </div>
-                  </div>
-                  <input
-                    name="tel"
-                    type="text"
-                    className="w-100"
-                    onChange={handleInputChange}
-                    value={inputs.tel}
-                  />
-                </div>
-                <div className="w-100"></div>
-                <div className="col-12 col-md-10 mt-2">
-                  <label className="d-block">Email:</label>
-                  <input
-                    name="email"
-                    type="email"
-                    className="w-100"
-                    autoComplete="on"
-                    required
-                    onChange={handleInputChange}
-                    value={inputs.email}
-                    onBlur={() => validateForm('email')}
-                  />
-                </div>
+                <label htmlFor="tel">Teléfono:</label>
+                <input type="text" id="tel" name="tel" ref={register} />
+                <label htmlFor="email">Email: *</label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  ref={register({ required: true })}
+                />
                 {errors.email && (
-                  <div className="col-12 col-md-10 mt-2 error">
-                    <p className="my-0">
-                      <span>
-                        Por favor ingrese una dirección de email válida.
-                      </span>
-                    </p>
-                    <p className="my-0">
-                      <span>
-                        Ej. <i>nombreapellido@gmail.com</i>
-                      </span>
-                    </p>
+                  <div className="error">
+                    <span>El campo de Email es requerido</span>
                   </div>
                 )}
-                <div className="w-100"></div>
-                <div className="col-12 col-md-10 mt-2">
-                  <label className="d-block">Asunto:</label>
-                  <input
-                    name="subject"
-                    type="text"
-                    className="w-100"
-                    autoComplete="on"
-                    onChange={handleInputChange}
-                    value={inputs.subject}
-                    onBlur={() => validateForm('subject')}
-                  />
-                </div>
-                <div className="w-100"></div>
-                <div className="col-12 col-md-10 mt-2">
-                  <label className="d-block">Mensaje:</label>
-                  <textarea
-                    name="message"
-                    className="w-100"
-                    required
-                    onChange={handleInputChange}
-                    value={inputs.message}
-                    onBlur={() => validateForm('message')}
-                  />
-                </div>
+                <label htmlFor="subject">Asunto:</label>
+                <input type="text" id="subject" name="subject" ref={register} />
+                <label htmlFor="message">Mensaje: *</label>
+                <textarea
+                  name="message"
+                  id="message"
+                  ref={register({ required: true, minLength: 10 })}
+                />
                 {errors.message && (
-                  <div className="col-12 col-md-10 mt-2 error">
+                  <div className="error">
                     <span>
                       El mensaje tiene que tener al menos{' '}
                       <span className="negrita">10 letras</span>
                     </span>
                   </div>
                 )}
-
-                {/* Error de cuando se envio el mail correctamente */}
                 {userMessage && (
                   <div className="col-12 col-md-10 mt-2 error">
                     {userMessage.type === 'success' ? (
@@ -261,16 +126,13 @@ const Contact = (props) => {
                     )}
                   </div>
                 )}
-                <div className="w-100"></div>
-                <div className="col-12 col-md-10 mt-2">
-                  {showSpinner ? (
-                    <div className="send-message">
-                      <p>{spinnerTextRef.current}</p>
-                    </div>
-                  ) : (
-                    <button type="subtmi">Enviar Mensaje</button>
-                  )}
-                </div>
+                {showSpinner ? (
+                  <div className="send-message">
+                    <p>{spinnerTextRef.current}</p>
+                  </div>
+                ) : (
+                  <button type="subtmi">Enviar Mensaje</button>
+                )}
               </form>
             </div>
             <div className="col-12 col-md-6 about-us-text">
